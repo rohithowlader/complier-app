c8d6c20450fff96b33c4f68b111886a777bd9edf
Rohit Howlader <rohithowlader2017@gmail.com>
2023-04-23 22:44:13 +0530

    Bug Fixes

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,85 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';import Head from "next/head";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "./Component/Navbar.js";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const state = {
+    button: 1,
+  };
+  const [code, setCode] = useState("");
+  // const [language, setLanguage] = useState("");
+  // const handleSubmit = async () => {
+  //   setIsLoading(true);
+  //   try {
+  //     const config = {
+  //       headers: {
+  //         "content-type": "application/json",
+  //       },
+  //     };
+  //     const dataToBeFed1 = {
+  //       code: code,
+  //     };
+  // axios
+  //   .post(
+  //     "http://localhost:5000/v1.0/getNodeCode",
+  //     dataToBeFed1,
+  //   )
+  //   .then((response) => {
+  //     console.log("Program Entered "), window.location.reload();
+
+  //   })
+
+  //   .catch((error) => {
+  //     console.log(dataToBeFed1);
+  //     console.error("There was an error!" + error);
+  //   });
+  //   } catch (error) {
+  //     console.log(error);
+  //     setCode("");
+  //   }
+  // };
+  const onSubmit = (e) => {
+    try {
+      const config = {
+        headers: {
+          "content-type": 'text/plain',
+        },
+      };
+
+      const dataToBeFed1 = {
+        code,
+      };
+      if (state.button === 1) {
+        console.log("Button 1 clicked!");
+        axios
+          .post("http://localhost:5000/v1.0/getNodeCode", code,config)
+          .then((response) => {
+            console.log("Program Entered "), window.location.reload();
+          })
+
+          .catch((error) => {
+            console.log(dataToBeFed1);
+            console.error("There was an error!" + error);
+          });
+      }
+      if (state.button === 2) {
+        console.log("Button 2 clicked!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -32,24 +103,55 @@ export default function Home() {
             }}
             maxRows={12}
           /> */}
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
+          {/*           
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            > */}
+          <form>
             <TextField
               id="outlined-multiline-static"
-              label="Multiline"
-              style = {{width: 850}}
+              label="Enter Code Here..."
+              style={{ width: 850 }}
               multiline
               rows={14}
-              defaultValue="Default Value"
               fullWidth
+              onChange={(event) => setCode(event.currentTarget.value)}
             />
-          </Box>
+            <button
+              onClick={() => (state.button = 1)}
+              type="submit"
+              name="node"
+              value="node"
+            ></button>
+          </form>
+          {/* <Button width="full" mt={4} type="submit">
+                Submit
+              </Button> */}
+
+          {/* </Box> */}
+          <form className="App" onSubmit={onSubmit}>
+            <button
+              onClick={() => (state.button = 1)}
+              type="submit"
+              name="btn1"
+              value="wow"
+            >
+              Button 1
+            </button>
+            <button
+              onClick={() => (state.button = 2)}
+              type="submit"
+              name="btn2"
+              value="oh no"
+            >
+              Button 2
+            </button>
+          </form>
         </div>
       </main>
     </>

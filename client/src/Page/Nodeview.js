2f342f7259b04c1e316b58dd252192f0b4854a81
Rohit Howlader <rohithowlader2017@gmail.com>
2023-05-13 00:01:55 +0530

    Node complier Ready

--- client/src/Page/Nodeview.js
+++ client/src/Page/Nodeview.js
@@ -1,9 +1,52 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 const Nodeview = () => {
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = async () => {
+    console.log(code);
+    const config = {
+      headers: {
+        "content-type": "text/plain",
+      },
+    };
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/v1.0/getNodeCode",
+        code,
+        config
+      );
+      console.log(data);
+      setMessage(data.message);
+      setOutput(data.output);
+      setError(data.stdout);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
-      <div>Node view</div>
+      <div className="App">Hello</div>
+      <h1>
+        Online Code Compiler
+        <br />
+        <textarea
+          rows="20"
+          cols="75"
+          valus={code}
+          onChange={(e) => {
+            setCode(e.target.value);
+          }}
+        ></textarea>
+        <br />
+        <button onClick={handleSubmit}>Submit</button>
+        <p>{message}</p>
+        <p>{output}</p>
+        <p>{error}</p>
+      </h1>
     </>
   );
 };

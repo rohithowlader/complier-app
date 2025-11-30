1cef57f230924823caf8ad435ea99d4a1dcfc9c8
Rohit Howlader <rohithowlader2017@gmail.com>
2025-11-30 14:39:11 +0530

    changed PORT

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.get("*", (req, res) => {
 });
 
 // ----- Port for Render -----
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`App is running on port : ${PORT}`);
